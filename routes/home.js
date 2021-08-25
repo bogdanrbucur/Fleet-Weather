3a25a12298539f83525aa9c491a46bf51faa2caf
Bogdan Bucur <bogdanrbucur@gmail.com>
2021-08-25 15:47:36 +0300

    implement pug render of ships array

--- routes/home.js
+++ routes/home.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const ships = require("../app");
 
 router.get("/", (req, res) => {
+  console.log("Client connected...");
   res.render("index", {
     // use to render HTML using a template engine like pug
     title: "My Express App",
-    ships: JSON.stringify(ships),
+    ships: ships,
   });
 });
 
--- views/script.js
+++ views/script.js
@@ -1,3 +1,7 @@
+(function () {
+  alert("I am here");
+})();
+
 var ships = JSON.parse(JSON.stringify(ships));
 
 tableView = document.getElementById("tableView");
