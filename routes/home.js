15454d94738d73f9ad352aa952c8d88618833298
Bogdan Bucur <bogdanrbucur@gmail.com>
2021-08-30 10:38:54 +0300

    router logging

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const ships = require("../app");
+const debug = require("debug")("app:route");
 
 router.get("/", (req, res) => {
-  console.log("Client connected.");
+  let ip = req.hostname;
+  debug(`Remote client connected ${ip}`);
   res.render("index", {
     // use to render HTML using a template engine like pug
     title: "Fleet Weather", // used in Pug
@@ -12,7 +14,8 @@ router.get("/", (req, res) => {
 });
 
 router.get("/api/getships", (req, res) => {
-  console.log("Client requested ships status update.");
+  let ip = req.ips;
+  debug(`Remote client ${ip} requested update.`);
   res.status(200).send(ships.ships); // send the ships array to the client
 });
 
