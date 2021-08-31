8db1d9b02bb8d6f6ad081ee9cf4c82a5a189320e
Bogdan Bucur <bogdanrbucur@gmail.com>
2021-08-31 11:42:06 +0300

    changed callbacks to promises

--- getShipInfo.js
+++ getShipInfo.js
@@ -3,35 +3,39 @@
 const puppeteer = require("puppeteer");
 const debug = require("debug")("app:getShipInfo");
 
-function getShipInfo(name, imo, callback) {
-  name = name.replace(" ", "-"); // For using the name in the URL
-  name = name.toUpperCase(); // For using the name in the URL
+function getShipInfo(name, imo) {
+  // function returns a promise
+  return new Promise((resolve, reject) => {
+    name = name.replace(" ", "-"); // For using the name in the URL
+    name = name.toUpperCase(); // For using the name in the URL
+  
+    const url = `https://www.vesselfinder.com/vessels/${name}-IMO-${imo}`;
+  
+    (async () => {
+      try {
+      const browser = await puppeteer.launch();
+      debug(`Puppeteer launch for VesselFinder.`);
+      const page = await browser.newPage();
+      await page.setUserAgent(
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+      );
+      await page.goto(url, { waitUntil: "networkidle0" }); // Wait for the page to fully load
+      debug(`Opened ${url}`);
+  
+      // Get text where the vessel position is written (div class="text2")
+      shipInfoText = await page.$eval(".text2", (el) => el.innerText);
+      resolve(shipInfoText); // Get the text from the element
+      debug(`Got text: ${shipInfoText}`);
+  
+      await browser.close();
+      debug(`Closed VesselFinder.`);
+      }
+      catch(e) {
+        debug("ERROR: ", e);
+      }
+    })();
+  });
 
-  const url = `https://www.vesselfinder.com/vessels/${name}-IMO-${imo}`;
-
-  (async () => {
-    try {
-    const browser = await puppeteer.launch();
-    debug(`Puppeteer launch for VesselFinder.`);
-    const page = await browser.newPage();
-    await page.setUserAgent(
-      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
-    );
-    await page.goto(url, { waitUntil: "networkidle0" }); // Wait for the page to fully load
-    debug(`Opened ${url}`);
-
-    // Get text where the vessel position is written (div class="text2")
-    shipInfoText = await page.$eval(".text2", (el) => el.innerText);
-    callback(shipInfoText); // Get the text from the element
-    debug(`Got text: ${shipInfoText}`);
-
-    await browser.close();
-    debug(`Closed VesselFinder.`);
-    }
-    catch(e) {
-      debug("ERROR: ", e);
-    }
-  })();
 }
 
 module.exports = getShipInfo;
--- vessel.js
+++ vessel.js
@@ -21,27 +21,32 @@ class Vessel {
   }
 
   update() {
-    getShipInfo(this.name, this.imo, (shipInfoText) => {
-      // shipInfoText is text from the VesselFinder page where info about the ship is located
-      // update vessel properties based on scraped data using parse functions from parse module
-      this.area = getArea(shipInfoText);
-      this.coordinates = getCoordinates(shipInfoText);
-      this.speed = getSpeed(shipInfoText);
-      this.destination = getDestination(shipInfoText);
-      this.eta = getETA(shipInfoText);
-      this.dataAge = getAge(shipInfoText);
+    // getShipInfo returns a Promise. When the promise is resolved, use the result to update the ship info
+    getShipInfo(this.name, this.imo)
+      .then((shipInfoText) => {
+        // shipInfoText is text from the VesselFinder page where info about the ship is located
+        // update vessel properties based on scraped data using parse functions from parse module
+        this.area = getArea(shipInfoText);
+        this.coordinates = getCoordinates(shipInfoText);
+        this.speed = getSpeed(shipInfoText);
+        this.destination = getDestination(shipInfoText);
+        this.eta = getETA(shipInfoText);
+        this.dataAge = getAge(shipInfoText);
 
-      // Scrape Windy for weather
-      getWeather(this.coordinates, (weather) => {
-        // weather is an array with all wind gusts shown on the page
-        debug(`Got weather:${weather}`);
-        debug(`Wind now: ${weather[3]}`);
-        debug(`Wind 6H: ${weather[5]}`);
-        this.windNow = weather[3]; // Corresponds to the wind gusts now in Windy
-        this.wind6H = weather[5]; // Corresponds to the wind gusts in 6H in Windy
-        debug(this);
-      });
-    });
+        // Scrape Windy for weather
+        getWeather(this.coordinates)
+          .then((weather) => {
+            // weather is an array with all wind gusts shown on the page
+            debug(`Got weather:${weather}`);
+            debug(`Wind now: ${weather[3]}`);
+            debug(`Wind 6H: ${weather[5]}`);
+            this.windNow = weather[3]; // Corresponds to the wind gusts now in Windy
+            this.wind6H = weather[5]; // Corresponds to the wind gusts in 6H in Windy
+            debug(this);
+          })
+          .catch((err) => debug(err.message));
+      })
+      .catch((err) => debug(err.message));
   }
 }
 
