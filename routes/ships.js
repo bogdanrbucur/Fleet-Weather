ac37b72a0e33c6a340050d998d5bd62fef395c09
Bogdan Bucur <bogdanrbucur@gmail.com>
2021-09-03 16:16:15 +0000

    implement Joi for data validation

--- routes/ships.js
+++ routes/ships.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const { getShips } = require("../mongodb");
 const debug = require("debug")("app:router_Ships");
 const { parseIP } = require("../parse"); // Import parseIP function from parse module
+const Joi = require("joi");
 
 // Get all ships - async because await is used
 router.get("/", async (req, res) => {
@@ -19,4 +20,14 @@ router.get("/", async (req, res) => {
   res.status(200).send(ships); // send the ships to the client
 });
 
+// Joi function to validate a Ship input
+function validateShip(ship) {
+  const schema = {
+    name: Joi.string().min(3).max(50).required(),
+    imo: Joi.number().integer().positive().min(7).max(7)
+  }
+  return Joi.validate(ship, schema);
+}
+
 module.exports = router;
+
