ad6dffbc12d5ea17058476544216877ee1468adb
Bogdan Bucur <bogdanrbucur@gmail.com>
2021-09-06 15:06:44 +0000

    cleanup

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const { port } = require("./startup/config");
 
 require("./startup/logging"); // Call the module that handled all logging
 require("./startup/routes")(app); // Call the module that houses all routes
-
-// Call function to continously cycle through all ships in array and update them
-require("./startup/updateLoop")();
+require("./startup/updateLoop")(); // Call function to continously cycle through all ships and update them
 
 // Read PORT from environment
 app.listen(port, () => winston.info(`Listening on port ${port}...`));
