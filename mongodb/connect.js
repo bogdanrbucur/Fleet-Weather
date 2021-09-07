62a25d67d9d953d0c20035f72d0c72cf4f532ce1
Bogdan Bucur <bogdanrbucur@gmail.com>
2021-09-07 13:07:28 +0000

    fix mongoose depreciation warnings

--- mongodb/connect.js
+++ mongodb/connect.js
@@ -5,7 +5,10 @@ const mongoose = require("mongoose");
 const { dbURL } = require("../startup/config");
 const debug = require("debug")("app:db-connect"); // $env:DEBUG="app:*" / export DEBUG="app:*" to see all debugs
 
-mongoose.connect(dbURL).then(() => winston.info(`Connected to ${dbURL}...`));
+mongoose
+  .connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true }) // launch options to get rid of depreciation warnings
+  .then(() => winston.info(`Connected to ${dbURL}...`));
+mongoose.set("useCreateIndex", true); // To get rid of a depreciation warning
 // .catch is handled on process level in Node
 
 module.exports = mongoose;
--- tests/integration/users.test.js
+++ tests/integration/users.test.js
@@ -11,21 +11,35 @@ describe("/api/users", () => {
     server.close(); // Close the server after each test
     await User.remove({}); // Clean the db
   });
-  describe("DELETE /:id", () => {
-    it("should delete requested user", async () => {
-      // Populate the db with some mock ships
-      let user = await User.collection.insertOne({
-        name: "test_user",
-        email: "test@email.com",
-      });
+  // describe("DELETE /:id", () => {
+  //   it("should delete requested user", async () => {
+  //     // Populate the db with some mock ships
+  //     let user = await User.collection.insertOne({
+  //       name: "test_user",
+  //       email: "test@email.com",
+  //     });
 
-      const res = await request(server).delete(`/api/users/${user._id}`);
-      expect(res.body.email === "test@email.com").toBeTruthy();
+  //     const res = await request(server).delete(`/api/users/${user._id}`);
+  //     expect(res.body.email === "test@email.com").toBeTruthy();
+  //   });
+  //   it("should return 404 if invalid ID is passed", async () => {
+  //     const res = await request(server).delete("/api/users/1");
+
+  //     expect(res.status).toBe(404);
+  //   });
+  // });
+  describe("POST /", () => {
+    it("should return return saved user", async () => {
+      const res = await request(server)
+        .post("/api/users")
+        .send({ name: "name1", email: "test@email1.com", password: "123456" });
+      expect(res.email).toBe("test@email1.com");
     });
-    it("should return 404 if invalid ID is passed", async () => {
-      const res = await request(server).delete("/api/users/1");
-
-      expect(res.status).toBe(404);
+    it("should return 401 if user format is invalid", async () => {
+      const res = await request(server)
+        .post("/api/users")
+        .send({ name: "name1", email: "test@email1.com" });
+      expect(res).toBe(400);
     });
   });
 });
