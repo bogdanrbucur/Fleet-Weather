88a34db7683a75b3f27112fde64b44d098d41bf1
Bogdan Bucur <bogdanrbucur@gmail.com>
2021-09-07 15:24:01 +0000

    implemenet auth middleware unit test

---
+++ tests/unit/middleware/auth.test.js
@@ -0,0 +1,23 @@
+const { User } = require("../../../models/user");
+const auth = require("../../../middleware/auth");
+const mongoose = require("mongoose");
+
+describe("auth middleware", () => {
+  it("should populate req.user with the payload of a valid JWT", () => {
+    const user = {
+      _id: mongoose.Types.ObjectId().toHexString(),
+      canModifyUsers: true,
+      canModifyShips: true,
+    };
+    const token = new User(user).generateAuthToken();
+    const req = {
+      header: jest.fn().mockReturnValue(token),
+    };
+    const res = {};
+    const next = jest.fn();
+
+    auth(req, res, next);
+
+    expect(req.user).toMatchObject(user);
+  });
+});
