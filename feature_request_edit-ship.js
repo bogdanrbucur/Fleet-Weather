4b27eaf31cdeb11af9208f1df74aaeec6507181a
Bogdan Bucur <bogdanrbucur@gmail.com>
2021-09-25 10:34:45 +0000

    attempt to extract text from Marine Traffic page

--- feature_request_edit-ship.js
+++
@@ -1,6 +0,0 @@
-// PUT /api/ships/:id
-
-
-// Set ship name
-// Set ship IMO number
-// Return modified ship
--- getShipInfo.js
+++ getShipInfo.js
@@ -7,32 +7,32 @@ const winston = require("winston");
 function getShipInfo(name, imo) {
   // function returns a promise
   return new Promise((resolve, reject) => {
-    name = name.replace(" ", "-"); // For using the name in the URL
-    name = name.toUpperCase(); // For using the name in the URL
-
-    const url = `https://www.vesselfinder.com/vessels/${name}-IMO-${imo}`;
+    const url = `https://www.marinetraffic.com/en/ais/details/ships/${imo}`;
 
     (async () => {
       const browser = await puppeteer.launch(); // Chromium on Raspberry Pi path
-      debug(`Puppeteer launch for VesselFinder.`);
+      debug(`Puppeteer launch for Marine Traffic.`);
       const page = await browser.newPage();
       await page.setUserAgent(
         "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
       ); // Pretend it's a real browser
       try {
-        await page.goto(url, { waitUntil: "networkidle2" }); // Wait for the page to fully load
+        await page.goto(url, { waitUntil: "networkidle0" }); // Wait for the page to fully load
         debug(`Opened ${url}`);
       } catch (ex) {
         winston.warn(ex.message);
       }
 
       // Get text where the vessel position is written (div class="text2")
-      shipInfoText = await page.$eval(".text2", (el) => el.innerText);
+      shipInfoText = await page.$eval(
+        ".MuiCollapse-wrapperInner",
+        (el) => el.innerText
+      );
       resolve(shipInfoText); // Get the text from the element
-      debug(`Got text from VesselFinder: ${shipInfoText}`);
+      debug(`Got text from Marine Traffic: ${shipInfoText}`);
 
       await browser.close();
-      debug(`Closed VesselFinder.`);
+      debug(`Closed Marine Traffic.`);
     })();
   });
 }
