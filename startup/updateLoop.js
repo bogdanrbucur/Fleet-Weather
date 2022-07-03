d71786397149477212052306e3457062a2e19e47
Bogdan Bucur <bogdanrbucur@gmail.com>
2022-07-03 11:04:04 +0000

    more comments

--- startup/updateLoop.js
+++ startup/updateLoop.js
@@ -10,16 +10,19 @@ module.exports = function updateShips() {
     const ships = await getShips(); // Get all ships in DB in every cycle
 
     let intervalToUpdateAllShips = 180; // minutes within which to update all ships
-    let interval = (intervalToUpdateAllShips / ships.length) * 60 * 1000;
+    let interval = (intervalToUpdateAllShips / ships.length) * 60 * 1000; // interval in ms between each vessel
 
+    // Every new cycle log the uptime and memory usage
     if (i === ships.length) {
       i = 0;
       winston.info(quickStats()); // App uptime and total memory usage. Pesky memory leaks...
     }
+
     debug(`Updating ship ${ships[i].name}.`);
     updateShip(ships[i]._id);
     i++;
 
+    // wait interval ms between vessels, then run the function again
     setTimeout(() => {
       updateLoop(i);
     }, interval);
